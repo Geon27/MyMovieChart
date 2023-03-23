f7e89a8371e5b87da8317c59bb29b0e95c1934cd
Geon_27 <yukgeon98527@gmail.com>
2023-03-24 03:33:05 +0900

    23/03/24

--- MyMovieChart/ListViewController.swift
+++ MyMovieChart/ListViewController.swift
@@ -19,5 +19,16 @@ class ListViewController: UITableViewController {
         mvo.rating = 8.95
         // 배열에 추가
         self.list.append(mvo)
+        
+        // 두번째 행
+        mvo = MoiveVO( )
+        mvo.title = "호우시절"
+        mvo.description = "때를 알고 내리는 좋은 비"
+        mvo.opendate = "2009-10-18"
+        mvo.rating = 7.31
+        // 배열에 추가
+        self.list.append(mvo)
+        
+        // 세번째 행
     }
 }
