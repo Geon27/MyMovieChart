cd20643e3ca9c904c2f980c4a5edcfa100561a2a
Geon_27 <yukgeon98527@gmail.com>
2023-04-24 01:00:20 +0900

    23/04/24 code added

--- MyMovieChart/MovieCell.swift
+++ MyMovieChart/MovieCell.swift
@@ -8,4 +8,12 @@
 import UIKit
 class MovieCell: UITableViewCell {
     
+    
+    @IBOutlet var title: UILabel!
+    
+    @IBOutlet var opendate: UILabel!
+    
+    @IBOutlet var rating: UILabel!
+    
+    @IBOutlet var desc: UILabel!
 }
